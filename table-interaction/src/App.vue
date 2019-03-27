c542ffa61d60d198e910ef5833aeedcda9d6f2f2
Link <lynk0613@hotmail.com>
2019-03-27 11:41:52 -0400

    add function, style

--- table-interaction/src/App.vue
+++ table-interaction/src/App.vue
@@ -34,15 +34,41 @@ export default {
   width: 300px;
   height: 500px;
   border-right: 1px gray solid;
+  background-color: #e6e6e6;
 }
 .panel-query {
   width: 100%;
 }
 .btn-tooltip {
-  font-size: 8px !important;
+  font-size: 6px !important;
   border-radius: 50% !important;
   padding: 0 !important;
-  width: 14px !important;
-  height: 14px !important;
+  width: 10px !important;
+  height: 10px !important;
+}
+.item-label {
+  font-size: 12px;
+}
+.item-wrapper {
+  border-radius: 2px;
+  border: 1px solid #e6e6e6;
+  min-height: 30px;
+}
+.item-wrapper div{
+  min-height: 15px;
+}
+.item-content {
+  background-color: lightblue;
+  border-radius: 2px;
+  border: 1px solid rgb(99, 99, 194);
+  display: flex;
+  margin: 2px;
+}
+.item-model {
+  background-color: white;
+  font-size: 10px;
+  margin: 3px;
+  padding: 1px 5px;
+  border-radius: 2px;
 }
 </style>
--- table-interaction/src/components/ModelQueryPanel.vue
+++ table-interaction/src/components/ModelQueryPanel.vue
@@ -1,27 +1,30 @@
 <template>
   <div class="panel">
     <div class="panel-model">
-      <b-form-input type="text" placeholder="Enter your name" />
-      <div v-b-toggle.collapse1 class="item-root"> 
-        > 
-        <span> Test Task Data </span>
+      <b-form-input type="text" placeholder="Enter your name"/>
+      <div v-b-toggle.collapse1 class="item-root">
+        >
+        <span>Test Task Data</span>
       </div>
       <b-collapse id="collapse1" class="mt-2">
-        <div class="item-wrapper">Meaures</div>
+        <div class="item-label">Meaures</div>
         <draggable v-model="measuresSrc" :group="{ name: 'measures', pull: 'clone', put: false}">
           <transition-group>
             <div v-for="(element, index) in measuresSrc" :key="'sm'+index" class="item-model">
               #
-              <span> {{ element.id }} </span>
+              <span>{{ element.id }}</span>
             </div>
           </transition-group>
         </draggable>
-        <div class="item-wrapper">Dimensions</div>
-        <draggable v-model="dimensionsSrc" :group="{ name: 'dimensions', pull: 'clone', put: false}">
+        <div class="item-label">Dimensions</div>
+        <draggable
+          v-model="dimensionsSrc"
+          :group="{ name: 'dimensions', pull: 'clone', put: false}"
+        >
           <transition-group>
             <div v-for="(element, index) in dimensionsSrc" :key="'sd'+index" class="item-model">
               A
-              <span> {{ element.id }} </span>
+              <span>{{ element.id }}</span>
             </div>
           </transition-group>
         </draggable>
@@ -31,47 +34,56 @@
       <b-tabs card>
         <b-tab title="Tab 1" active>
           <b-card-text>
-          <div class="item-wrapper">
-            Meaures
-            <b-button v-b-tooltip.hover title="Tooltip content2" class="btn-tooltip">?</b-button>
-          </div>
-          <draggable v-model="meansuresQue" group="measures">
-            <transition-group>
-              <div v-for="(element, index) in meansuresQue" :key="'qm'+index" class="item-model">
-                #
-                <span> {{ element.id }} </span>
-              </div>
-            </transition-group>
-          </draggable>
+            <div class="item-label">
+              Meaures
+              <b-button v-b-tooltip.hover title="Tooltip content2" class="btn-tooltip">?</b-button>
+            </div>
+            <div class="item-wrapper">
+              <draggable v-model="measuresQue" group="measures" @change="changeMeasures">
+                <transition-group>
+                  <div v-for="(element, index) in measuresQue" :key="'qm'+index" class="item-content">
+                    <div class="item-model">
+                      #
+                      <span>{{ element.id }}</span>
+                    </div>
+                  </div>
+                </transition-group>
+              </draggable>
+            </div>
 
-          <div class="item-wrapper">
-            Dimensions
-            <b-button v-b-tooltip.hover title="Tooltip content2" class="btn-tooltip">?</b-button>  
-          </div>
-          <draggable v-model="dimensionsQue" group="dimensions" @change="changeDimensions">
-            <transition-group>
-              <div v-for="(element, index) in dimensionsQue" :key="'qd'+index" class="item-model">
-                A
-                <span> {{ element.id }} </span>
-              </div>
-            </transition-group>
-          </draggable>
+            <div class="item-label">
+              Dimensions
+              <b-button v-b-tooltip.hover title="Tooltip content2" class="btn-tooltip">?</b-button>
+            </div>
+            <div class="item-wrapper">
+              <draggable v-model="dimensionsQue" group="dimensions" @change="changeDimensions">
+                <transition-group>
+                  <div v-for="(element, index) in dimensionsQue" :key="'qd'+index" class="item-content">
+                    <div class="item-model">
+                      A
+                      <span>{{ element.id }}</span>
+                    </div>
+                  </div>
+                </transition-group>
+              </draggable>
+            </div>
 
-
-
-          <div class="item-wrapper">
-            Filters
-            <b-button v-b-tooltip.hover title="Tooltip content" class="btn-tooltip">?</b-button>
-          </div>
-          <draggable v-model="filtersQue" group="dimensions" @change="changeFilters">
-            <transition-group>
-              <div v-for="(element, index) in filtersQue" :key="'qf'+index" class="item-model">
-                A
-                <span> {{ element.id }} </span>
-              </div>
-            </transition-group>
-          </draggable>
-
+            <div class="item-label">
+              Filters
+              <b-button v-b-tooltip.hover title="Tooltip content" class="btn-tooltip">?</b-button>
+            </div>
+            <div class="item-wrapper">
+              <draggable v-model="filtersQue" group="dimensions" @change="changeFilters">
+                <transition-group>
+                  <div v-for="(element, index) in filtersQue" :key="'qf'+index" class="item-content">
+                    <div class="item-model">
+                      A
+                      <span>{{ element.id }}</span>
+                    </div>
+                  </div>
+                </transition-group>
+              </draggable>
+            </div>
           </b-card-text>
         </b-tab>
         <b-tab title="Tab 2">
@@ -103,35 +115,33 @@ export default {
         { id: "Region", label: "Region" },
         { id: "Scenario", label: "Scenario" }
       ],
-      meansuresQue: [
-        { id: "Model", label: "Model" },
-        { id: "Period", label: "Period" },
-        { id: "Region", label: "Region" },
-        { id: "Scenario", label: "Scenario" }
-      ],
-      dimensionsQue: [
-        { id: "Model", label: "Model" },
-        { id: "Period", label: "Period" },
-        { id: "Region", label: "Region" },
-        { id: "Scenario", label: "Scenario" }
-      ],
-      filtersQue: [
-        { id: "Model", label: "Model" },
-        { id: "Period", label: "Period" },
-        { id: "Region", label: "Region" },
-        { id: "Scenario", label: "Scenario" }
-      ]
+      measuresQue: [],
+      dimensionsQue: [],
+      filtersQue: []
     };
   },
   methods: {
+    changeMeasures(e) {
+      if (e.added) {
+        let dup = false;
+        this.measuresQue.forEach((element, index) => {
+          if (element.id === e.added.element.id && index !== e.added.newIndex)
+            dup = true;
+        });
+        if (dup) {
+          this.removeMeasures(e.added.newIndex);
+        }
+      }
+    },
     changeDimensions(e) {
       if (e.added) {
         let dup = false;
         this.dimensionsQue.forEach((element, index) => {
-          if (element.id === e.added.element.id && index !== e.added.newIndex) dup = true;
+          if (element.id === e.added.element.id && index !== e.added.newIndex)
+            dup = true;
         });
         if (dup) {
-          this.dimensionsQue.splice(e.added.newIndex, 1);
+          this.removeDimensions(e.added.newIndex);
         }
       }
     },
@@ -139,14 +149,23 @@ export default {
       if (e.added) {
         let dup = false;
         this.filtersQue.forEach((element, index) => {
-          if (element.id === e.added.element.id && index !== e.added.newIndex) dup = true;
+          if (element.id === e.added.element.id && index !== e.added.newIndex)
+            dup = true;
         });
         if (dup) {
-          this.filtersQue.splice(e.added.newIndex, 1);
+          this.removeFilters(e.added.newIndex);
         }
       }
+    },
+    removeMeasures(index) {
+      this.measuresQue.splice(index, 1);
+    },
+    removeDimensions(index) {
+      this.dimensionsQue.splice(index, 1);
+    },
+    removeFilters(index) {
+      this.filtersQue.splice(index, 1);
     }
-
   }
 };
 </script>
